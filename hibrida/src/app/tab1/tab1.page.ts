d9d79fef2aeecab751af87b7dee9365405cdad82
paulethTandazo <117050687+paulethTandazo@users.noreply.github.com>
2025-01-14 21:09:58 -0500

    cambios

--- hibrida/src/app/tab1/tab1.page.ts
+++ hibrida/src/app/tab1/tab1.page.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { 
+IonFab, IonFabButton, IonIcon, IonCard,  
+IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { addIcons } from 'ionicons';
+import { cloudUploadOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonFab, IonFabButton, IonIcon, IonCard, 
+    IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
 })
 export class Tab1Page {
-  constructor() {}
-}
+  constructor() {
+    /* Registre el ícono */
+    addIcons({ cloudUploadOutline });
+}}
